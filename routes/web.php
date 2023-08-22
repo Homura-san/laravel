17c89fa6549263e94711359654ff89cc3fa096da
Homura-san <erickbispo@ufba.br>
2023-08-22 15:30:40 -0300

    Rotas com parâmetros

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('clients', function() {
+    return '<h1> lista de clientes</h1>';
+});
+
+Route::get('clients/create/new', function() {
+    return '<h1> Criar clientes</h1>';
+});
+
+Route::get('clients/{name}/{age}', function ($name, $age) {
+    return "Detalhes do cliente {$name}, {$age} anos.";
+})->where('age', '[0-9]+');
