050e29ac90926dc5e83c393c68ae31d2a5260a3d
Homura-san <erickbispo@ufba.br>
2023-08-24 12:29:19 -0300

    Métodos http

--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,31 @@ Route::prefix('clients')->group(function () {
     
     Route::get('create/new', function() {
         $html = '<h1>Criar cliente</h1>';
+        
+        $link = route('clients.save');
+
+        $html .= "
+        <br>
+        <form method='POST' action='{$link}'>
+            <input type='hidden' name='_token' value='" . csrf_token() . "'>
+            <!-- <input type='hidden' name='_method' value='PUT'> Como usar método put, delete, etc -->
+            <input placeholder='insira seu nome'> <br>
+            <input placeholder='insira sua idade'> <br>
+            <button>Enviar</button>
+        </form>";
+        
         $html .= '<br><a href="' . route('clients.list') . '"> Lista de Clientes</a>';
         return $html;
     });
     
-    Route::get('{name}/{age}', function ($name, $age) {
-        $html =  "Detalhes do cliente {$name}, {$age} anos.";
-        $html .= '<br><a href="' . route('clients.list') . '"> Lista de Clientes</a>';
-        return $html;
-    })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);
+    Route::post('save', function () {
+        return 'Cliente criado com sucesso.';
+    })->name('clients.save');
+
+    // Route::get('{name}/{age}', function ($name, $age) {
+    //     $html =  "Detalhes do cliente {$name}, {$age} anos.";
+    //     $html .= '<br><a href="' . route('clients.list') . '"> Lista de Clientes</a>';
+    //     return $html;
+    // })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);
     
 });
