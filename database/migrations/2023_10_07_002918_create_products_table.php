ffe11e59ae02a3e9510c2acb9a800aba9d56262b
Homura-san <erickbispo@ufba.br>
2023-10-06 21:32:30 -0300

    Criando models

--- database/migrations/2023_10_07_002918_create_products_table.php
+++ database/migrations/2023_10_07_002918_create_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('create_products_table', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('create_products_table');
+        Schema::dropIfExists('products');
     }
 };
